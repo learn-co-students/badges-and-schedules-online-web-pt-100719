a1e47c802a18d104995afb58e97103f6e9291ca4
Artiium Arbyummi <artiium21@gmail.com>
2019-10-16 00:42:07 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  messages = []
+ names.each do | name | 
+   messages << "Hello, my name is #{name}."
+  end
+  return messages
+end
+
+def assign_rooms(names)
+  room = 1
+  new_message = []
+  names.each do | name |
+    new_message << "Hello, #{name}! You'll be assigned to room #{room}!"
+    room += 1 
+end
+
+return new_message
+
+end
+
+def printer(names)
+  puts printer.batch_badge_creator
+  
+   
+end
+
+
